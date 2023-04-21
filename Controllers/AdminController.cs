6ca8a01847471e025d632f9adfe0bbbc92322c6a
Francesco C <fran461j@outlook.it>
2023-04-21 16:23:48 +0200

    Index and Details

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using net_il_mio_fotoalbum.Models;
+
+namespace net_il_mio_fotoalbum.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            using(PhotoContext ctx = new PhotoContext())
+            {
+                Photo[] photos = ctx.Photos.ToArray();
+                return View(photos);
+            }
+        }
+
+        public IActionResult Details(int id)
+        {
+            using(PhotoContext ctx = new PhotoContext())
+            {
+                Photo photo = ctx.Photos.Where(photo => photo.Id == id).FirstOrDefault();
+                return View(photo);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Admin}/{action=Index}/{id?}");
 
 using(PhotoContext ctx = new PhotoContext())
 {
