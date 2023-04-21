2f9fb21d5e7ef181c5547a54882f7542166a53d5
Francesco C <fran461j@outlook.it>
2023-04-21 13:34:05 +0200

    Add project files.

---
+++ Models/Photo.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    [Table("photos")]
+    public class Photo
+    {
+        //attributes
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("title")]
+        [Required(ErrorMessage = "É necessario fornire un titolo.")]
+        public string Title { get; set; }
+
+        [Column("image")]
+        [Required(ErrorMessage = "É necessario fornire un'immagine.")]
+        public byte[] Image { get; set; }
+
+        [Column("description")]
+        public string Description { get; set; }
+
+        [Column("url")]
+        public string Url { get; set; }
+
+        [Column("visibility")]
+        public bool Visibility { get; set; }
+
+    }
+}
---
+++ Models/PhotoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    public class PhotoContext : DbContext
+    {
+        public DbSet<Photo> Photos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
